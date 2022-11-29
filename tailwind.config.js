df766b5218f7f77d4810e0b0b7e98fc182dfdaa1
Nando Schär <schaer.nando@gmail.com>
2022-11-29 20:51:53 +0100

    feat: add spacing style

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,15 @@ module.exports = {
       fontFamily: {
         'poppins': ['Poppins', 'sans-serif'],
       },
+      spacing: {
+        xxs: '4px',
+        xs: '8px',
+        s: '16px',
+        m: '24px',
+        l: '32px',
+        xl: '48px',
+        xxl: '64px'
+      }
     },
   },
   plugins: [
