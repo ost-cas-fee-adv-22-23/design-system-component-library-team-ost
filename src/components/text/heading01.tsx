e21f13fe744333d5bde424f97d46c1236950529e
martinthomann <mthomann@me.com>
2022-12-01 21:55:49 +0100

    Add heading font sizes to tailwind

---
+++ src/components/text/heading01.tsx
@@ -0,0 +1,11 @@
+import React, { FC, ReactNode } from 'react';
+
+export type HeadingProps = {
+  children: ReactNode;
+};
+
+export const HeadingH1: FC<HeadingProps> = ({ children }) => (
+  <h1 className={'text-h1 text-slate-600'}>
+    {children}
+  </h1>
+);
