276b974bb96ac42ea768842e8c8a783b9c3df562
martinthomann <mthomann@me.com>
2022-12-06 17:14:56 +0100

    Minor changes

--- src/components/text/heading.stories.tsx
+++ src/components/text/heading.stories.tsx
@@ -7,10 +7,6 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { Heading } from './heading';
 
 export default {
-  /* 👇 The title prop is optional.
-  * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
-  * to learn how to generate automatic titles
-  */
   title: 'Foundation/Typography',
   component: Heading,
 } as ComponentMeta<typeof Heading>;
