e25fa5202c68f9de8f629f17fc03fdfb85881bcc
Nando Schär <schaer.nando@gmail.com>
2022-12-14 14:34:46 +0100

    feat: color tokens fixiert

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,71 @@
-/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
 
+
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js, jsx, ts,tsx}'],
   theme: {
+    colors: {
+      current: 'currentColor',
+      transparent: 'transparent',
+      black: colors.black,
+      white: colors.white,
+      slate: {
+        50: '#f8fafc',
+        100: '#f1f5f9',
+        200: '#e2e8f0',
+        300: '#cbd5e1',
+        400: '#94a3b8',
+        500: '#64748b',
+        600: '#475569',
+        700: '#334155',
+        800: '#1e293b',
+        900: '#0f172a',
+      },
+      gray: colors.gray,
+      zinc: colors.zinc,
+      neutral: colors.neutral,
+      stone: colors.stone,
+      red: colors.red,
+      orange: colors.orange,
+      amber: colors.amber,
+      yellow: colors.yellow,
+      lime: colors.lime,
+      green: colors.green,
+      emerald: colors.emerald,
+      teal: colors.teal,
+      cyan: colors.cyan,
+      sky: colors.sky,
+      blue: colors.blue,
+      indigo: colors.indigo,
+      violet: {
+        50: '#f5f3ff',
+        100: '#ede9fe',
+        200: '#ddd6fe',
+        300: '#c4b5fd',
+        400: '#a78bfa',
+        500: '#8b5cf6',
+        600: '#7c3aed',
+        700: '#6d28d9',
+        800: '#5b21b6',
+        900: '#4c1d95',
+      },
+      purple: colors.purple,
+      fuchsia: colors.fuchsia,
+      pink: {
+        50: '#fdf2f8',
+        100: '#fce7f3',
+        200: '#fbcfe8',
+        300: '#f9a8d4',
+        400: '#f472b6',
+        500: '#ec4899',
+        600: '#db2777',
+        700: '#be185d',
+        800: '#9d174d',
+        900: '#831843',
+      },
+      rose: colors.rose,
+    },
     extend: {
       fontFamily: {
         poppins: ['Poppins', 'sans-serif'],
