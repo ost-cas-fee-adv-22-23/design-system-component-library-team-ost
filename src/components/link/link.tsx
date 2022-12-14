ef978f41ace1169a3f060170c6f5eb8fbe29624d
Nando Schär <schaer.nando@gmail.com>
2022-12-14 14:01:31 +0100

    feat: mergeClasses beim link implementiert

--- src/components/link/link.tsx
+++ src/components/link/link.tsx
@@ -1,10 +1,11 @@
-import { FC, ReactNode } from 'react';
+import { FC } from 'react';
 import { IconProfile } from '../icons/icon-profile';
 import { IconTime } from '../icons/icon-time';
 import { IconLocation } from '../icons/icon-location';
 import { IconCalendar } from '../icons/icon-calendar';
 import { IconSizes } from '../icons/icon-props';
 import { Label, LabelSizes } from '../text/label';
+import { mergeClassNames } from './../../helpers/merge-class-names';
 
 export enum LinkType {
   text = 'text',
@@ -18,11 +19,11 @@ export type LinkProps = {
   type: LinkType;
   href: string;
   target: '_blank' | '_self';
-  children: ReactNode;
+  children: string;
 };
 
 export const Link: FC<LinkProps> = ({ type, href, target = '_blank', children }) => {
-  const linkBaseStyle = ['flex items-center gap-xxs'];
+  const linkBaseStyle = ['flex', 'items-center', 'gap-xxs'];
 
   const linkVariantStyles = {
     text: ['text-violet-600', 'hover:decoration-violet-200', 'underline', 'underline-offset-2', 'hover:underline-offset-4'],
@@ -32,19 +33,16 @@ export const Link: FC<LinkProps> = ({ type, href, target = '_blank', children })
     joined: ['text-slate-400', 'hover:text-slate-600'],
   };
 
-  // todo: Icon Size in ein System bringen (z.B. XS-XL)
+  // todo: Können hier auch die Pfade angegeben werden und später die Icons lazy geladen werden?
   const linkVariantIcons = {
     text: '',
-    profile: <IconProfile size={ IconSizes.s } />,
-    timestamp: <IconTime size={ IconSizes.s } />,
-    location: <IconLocation size={ IconSizes.s } />,
-    joined: <IconCalendar size={ IconSizes.s } />,
+    profile: <IconProfile size={IconSizes.s} />,
+    timestamp: <IconTime size={IconSizes.s} />,
+    location: <IconLocation size={IconSizes.s} />,
+    joined: <IconCalendar size={IconSizes.s} />,
   };
 
-  /* todo ns: join durch globaler Helper ersetzen*/
-  // todo ns: Kann ein Icon dynamisch geladen werden?
-  // todo ns: Cursor Pointer beim Label mitgeben
-  const classes = [...linkBaseStyle, ...linkVariantStyles[type]].join(' ');
+  const classes = mergeClassNames([linkBaseStyle, linkVariantStyles[type]]);
 
   return (
     <a href={href} target={target} className={classes}>
