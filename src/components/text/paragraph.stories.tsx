76af8c4dadec38009da6cc3e31654abca63fc7b8
martinthomann <mthomann@me.com>
2022-12-16 21:29:33 +0100

    feat: precommit test for unused import

--- src/components/text/paragraph.stories.tsx
+++ src/components/text/paragraph.stories.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { ComponentStory, ComponentMeta, Story } from '@storybook/react';
 import { Paragraph, ParagraphSizes } from './paragraph';
 
 export default {
