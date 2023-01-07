37ced6bda841a3ec5a0f101d3af6d6b11827c3e2
Nando Schär <schaer.nando@gmail.com>
2023-01-07 12:32:52 +0100

    feat: refactoring UserShortRepresentation abgeschlossen

--- src/components/user/short-representation/user-short-representation.tsx
+++ src/components/user/short-representation/user-short-representation.tsx
@@ -22,78 +22,16 @@ export enum UserShortRepresentationLabelType {
   h3 = 'h3',
 }
 
-// type UserShortRepresentationProps = {
-//   size: UserShortRepresentationSizes;
-//   displayName?: string;
-//   src?: string;
-//   alt?: string;
-//   timestamp?: string;
-//   location?: string;
-//   joined?: string;
-//   username?: string;
-//   hrefProfile?: string;
-// } & ({ onSettingsClick: () => void } | { showSettings: boolean });
-
-// type UserShortRepresentationProps = {
-//   /**
-//    * Provides alternative information for the profile picture if a user for some reason cannot view it.
-//    */
-//   alt?: string;
-//   /**
-//    * Specifies the name to display in the label.
-//    */
-//   displayName?: string;
-//   /**
-//    * Specifies the URL of the page the profile icon link goes to.
-//    */
-//   hrefProfile?: string;
-//   /**
-//    * Specifies when the user has joined.
-//    */
-//   joined?: string;
-//   /**
-//    * Specifies the label which shows the displayName.
-//    */
-//   labelType: UserShortRepresentationLabelType;
-//   /**
-//    * Specifies the location where the user is from.
-//    */
-//   location?: string;
-//   /**
-//    * Specifies the action, when the user clicks on the settings icon.
-//    */
-//   onSettingsClick: () => void;
-//   /**
-//    * Specifies the size of the profile picture.
-//    */
-//   profilePictureSize?: UserShortRepresentationProfilePictureSize;
-//   /**
-//    * Specifies if the settings icon is visible.
-//    */
-//   showSettings: boolean;
-//   /**
-//    * Specifies the URL of the profile picture.
-//    */
-//   src?: string;
-//   /**
-//    * Specifies the timestamp when the user took action.
-//    */
-//   timestamp?: string;
-//   /**
-//    * Specifies the username.
-//    */
-//   username?: string;
-// };
-
 type UserShortRepresentationBaseProps = {
   /**
    * Provides alternative information for the profile picture if a user for some reason cannot view it.
+   * To display the profile picture the following properties are required: "profilePictureSize", "src" and "alt".
    */
   alt?: string;
   /**
    * Specifies the name to display in the label.
    */
-  displayName?: string;
+  displayName: string;
   /**
    * Specifies the URL of the page the profile icon link goes to.
    */
@@ -111,19 +49,22 @@ type UserShortRepresentationBaseProps = {
    */
   location?: string;
   /**
-   * Specifies the action, when the user clicks on the settings icon.
+   * Specifies the action, which is called as the user clicks on the settings icon.
+   * To display the settings icon the following properties are required: "showSettings" and "onSettingsClick".
    */
   onSettingsClick?: () => void;
   /**
-   * Specifies the size of the profile picture.
+   * Specifies the size of the profile picture. To display the profile picture the following properties are required: "profilePictureSize", "src" and "alt".
    */
   profilePictureSize?: UserShortRepresentationProfilePictureSize;
   /**
    * Specifies if the settings icon is visible.
+   * To display the settings icon the following properties are required: "showSettings" and "onSettingsClick".
    */
   showSettings?: boolean;
   /**
    * Specifies the URL of the profile picture.
+   * To display the profile picture the following properties are required: "profilePictureSize", "src" and "alt".
    */
   src?: string;
   /**
@@ -158,46 +99,37 @@ type WithSettingsProps = {
   onSettingsClick: () => void;
 };
 
-type A = UserShortRepresentationBaseProps & WithoutProfilePictureProps & WithoutSettingsProps;
-type B = UserShortRepresentationBaseProps & WithProfilePictureProps & WithoutSettingsProps;
-type C = UserShortRepresentationBaseProps & WithoutProfilePictureProps & WithSettingsProps;
-type D = UserShortRepresentationBaseProps & WithProfilePictureProps & WithSettingsProps;
+type BaseWithoutProfilePictureWithoutSettings = UserShortRepresentationBaseProps &
+  WithoutProfilePictureProps &
+  WithoutSettingsProps;
+type BaseWithProfilePictureWithoutSettings = UserShortRepresentationBaseProps &
+  WithProfilePictureProps &
+  WithoutSettingsProps;
+type BaseWithoutProfilePictureWithSettings = UserShortRepresentationBaseProps &
+  WithoutProfilePictureProps &
+  WithSettingsProps;
+type BaseWithProfilePictureWithSettings = UserShortRepresentationBaseProps & WithProfilePictureProps & WithSettingsProps;
 
-type UserShortRepresentationProps = A | B | C | D;
+type UserShortRepresentationProps =
+  | BaseWithoutProfilePictureWithoutSettings
+  | BaseWithProfilePictureWithoutSettings
+  | BaseWithoutProfilePictureWithSettings
+  | BaseWithProfilePictureWithSettings;
 
-export const UserShortRepresentation: FC<UserShortRepresentationProps> = (
-  props
-  //   {
-  //   alt,
-  //   displayName = 'Neuer User',
-  //   hrefProfile,
-  //   joined = 'Joined',
-  //   labelType,
-  //   location = 'Location',
-  //   onSettingsClick,
-  //   profilePictureSize,
-  //   showSettings,
-  //   src,
-  //   timestamp,
-  //   username = 'neueruser',
-  // }
-) => {
-  // typeguards?
-  const {
-    alt,
-    displayName,
-    hrefProfile,
-    joined,
-    labelType,
-    location,
-    onSettingsClick,
-    profilePictureSize,
-    showSettings,
-    src,
-    timestamp,
-    username,
-  } = props;
-
+export const UserShortRepresentation: FC<UserShortRepresentationProps> = ({
+  alt,
+  displayName,
+  hrefProfile,
+  joined,
+  labelType,
+  location,
+  onSettingsClick,
+  profilePictureSize,
+  showSettings,
+  src,
+  timestamp,
+  username,
+}) => {
   const profilePictureVariantStyles: Record<UserShortRepresentationProfilePictureSize, ProfilePictureSizes> = {
     s: ProfilePictureSizes.s,
     m: ProfilePictureSizes.m,
@@ -212,7 +144,7 @@ export const UserShortRepresentation: FC<UserShortRepresentationProps> = (
 
   return (
     <Stack spacing={StackSpacings.xs} alignitems={StackAlignItems.center}>
-      {profilePictureSize && alt && (
+      {profilePictureSize && src && alt && (
         <ProfilePicture size={profilePictureVariantStyles[profilePictureSize]} src={src} alt={alt} />
       )}
 
