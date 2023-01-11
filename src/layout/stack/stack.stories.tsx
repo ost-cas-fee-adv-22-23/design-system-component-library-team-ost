1e41f16e9f0eeb663995f2c0b7f68bb66f9e281e
Nando Schär <schaer.nando@gmail.com>
2023-01-11 17:19:06 +0100

    feat: add MumblesInTimeline Story

--- src/layout/stack/stack.stories.tsx
+++ src/layout/stack/stack.stories.tsx
@@ -1,6 +1,7 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
-import React from 'react';
-import { Stack, StackAlignItems, StackDirection, StackJustifyContent, StackSpacing } from './stack';
+import React, { FC } from 'react';
+import { MumbleInTimeline } from '../card/card.stories';
+import { Stack, StackAlignItems, StackDirection, StackJustifyContent, StackProps, StackSpacing } from './stack';
 
 export default {
   title: 'Layouts/Stack',
@@ -40,3 +41,47 @@ WithDivider.args = {
   spacing: StackSpacing.m,
   withDivider: true,
 };
+
+interface StackWithContainerWidth extends FC<StackProps> {
+  /**
+   * Specifies the width of the parent component. It's not a property of the Stack Component and only
+   * for demo purposes.
+   */
+  containerWidth: number;
+}
+
+const MumblesInTimelineTemplate: ComponentStory<StackWithContainerWidth> = (args) => {
+  MumbleInTimeline.args.containerWidth = (args as unknown as StackWithContainerWidth).containerWidth;
+
+  return (
+    <div style={{ width: (args as unknown as StackWithContainerWidth).containerWidth + 'px' }}>
+      <Stack direction={StackDirection.col} spacing={StackSpacing.s}>
+        {[...Array(7)].map((value: undefined, index: number) => (
+          <MumbleInTimeline id={index + 1} key={index} {...MumbleInTimeline.args}></MumbleInTimeline>
+        ))}
+      </Stack>
+    </div>
+  );
+};
+export const MumblesInTimeline = MumblesInTimelineTemplate.bind({});
+MumblesInTimeline.args = {
+  containerWidth: 680,
+};
+MumblesInTimeline.argTypes = {
+  alignItems: { table: { disable: true } },
+  children: { table: { disable: true } },
+  containerWidth: {
+    control: { type: 'range', min: 200, max: 1000, step: 10 },
+    description:
+      "Specifies the width of the parent component. It's not a property of the Stack Component and only for demo purposes.",
+  },
+  direction: { table: { disable: true } },
+  justifyContent: { table: { disable: true } },
+  spacing: { table: { disable: true } },
+  withDivider: { table: { disable: true } },
+};
+MumblesInTimeline.parameters = {
+  backgrounds: { default: 'background' },
+  layout: 'centered',
+};
+MumblesInTimeline.storyName = '🐼 Mumbles In Timeline';
