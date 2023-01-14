8c395922f39bf84b7c069aa648513356386d2b21
martinthomann <mthomann@me.com>
2023-01-14 22:13:02 +0100

    feat: add textarea to example form with aria-label

--- src/layout/page-header/page-header.stories.tsx
+++ src/layout/page-header/page-header.stories.tsx
@@ -12,6 +12,7 @@ import { Label, LabelSize } from '../../components/typography/label';
 import { Form } from '../../components/form/form';
 import { Stack, StackDirection, StackSpacing } from '../stack/stack';
 import { Input, InputTypes } from '../../components/form/form-input';
+import { Textarea } from '../../components/form/form-textarea';
 import { TextButton, TextButtonColor, TextButtonSize, TextButtonDisplayMode } from '../../components/buttons/text-button';
 import { IconCancel } from '../../components/icons/icon-cancel';
 import { IconCheckmark } from '../../components/icons/icon-checkmark';
@@ -46,7 +47,7 @@ export const MumblePageHeader: ComponentStory<typeof PageHeader> = () => {
     biography: '',
   });
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     setForm({
       ...form,
       [e.target.name]: e.target.value,
@@ -57,7 +58,7 @@ export const MumblePageHeader: ComponentStory<typeof PageHeader> = () => {
     <PageHeader>
       <div className="flex items-center justify-between gap-56">
         <div className="h-10">
-          <MumbleWhiteHorizontal onClick={action('onLogoClick')} />
+          <MumbleWhiteHorizontal ariaLabel="Go to mumble" onClick={action('onLogoClick')} />
         </div>
         <Navigation>
           <ProfilePictureButton
@@ -105,12 +106,13 @@ export const MumblePageHeader: ComponentStory<typeof PageHeader> = () => {
             type={InputTypes.text}
             value={form.city}
           />
-          <Input
+          <Textarea
+            ariaLabel="biography"
             label="Biografie"
             labelSize={LabelSize.s}
             name="biography"
             onChange={handleChange}
-            type={InputTypes.text}
+            rows={2}
             value={form.biography}
           />
           <Stack direction={StackDirection.row} spacing={StackSpacing.xs}>
