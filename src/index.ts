53943f7d910ab17135001a0f48d7d4a71eafb162
martinthomann <mthomann@me.com>
2023-02-18 21:02:46 +0100

    fix: export card public-api

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ export * from './components/profile-picture/public-api';
 export * from './components/tab-nav/public-api';
 export * from './components/typography/public-api';
 export * from './components/user/public-api';
+export * from './layout/card/public-api';
 export * from './layout/navigation/public-api';
 export * from './layout/page-header/public-api';
 export * from './layout/stack/public-api';
